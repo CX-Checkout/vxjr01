3be3d535be27bf6410dbefa1ab09f1964c65ccc9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 12:19:57 +0000

    Sun Dec 03 12:19:57 GMT 2017

--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -1,5 +1,22 @@
-import static org.junit.Assert.*;
+package befaster.solutions;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
 
 public class HelloTest {
+
+    @Test
+    public void should_return_hello_world() {
+        assertThat(Hello.hello("Robert"), is("Hello Robert!"));
+        assertThat(Hello.hello("Mirek"), is("Hello Mirek!"));
+    }
+
+    @Test
+    public void should_return_hello_world_when_name_is_undefined() {
+        assertThat(Hello.hello(""), is("Hello World!"));
+        assertThat(Hello.hello(null), is("Hello World!"));
+    }
 
 }
