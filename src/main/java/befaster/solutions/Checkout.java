2fac11167a156553c2b6b44760f572f9aad57860
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 13:20:49 +0000

    Sun Dec 03 13:20:49 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,9 +1,12 @@
 package befaster.solutions;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
 
 public class Checkout {
     private static final String A_SKU = "A";
@@ -19,7 +22,8 @@ public class Checkout {
 
     public static Integer checkout(String skus) {
         List<String> listOfSkus = asList(skus.split(""));
-
+        Long numberOfBSkusInBasket = listOfSkus.stream()
+                .collect(groupingBy(Function.identity(), counting())).get(B_SKU);
 
         return (listOfSkus).stream()
                 .mapToInt((String sku) -> priceMap.get(sku))
