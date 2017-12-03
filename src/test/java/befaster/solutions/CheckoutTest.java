af979f3a3dca6f467f470b664e9b416a9fb8b118
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 20:13:59 +0000

    Sun Dec 03 20:13:59 GMT 2017

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -12,11 +12,13 @@ public class CheckoutTest {
     private static final String C_SKU = "C";
     private static final String D_SKU = "D";
     private static final String E_SKU = "E";
+    private static final String F_SKU = "F";
     private static final int A_SKU_PRICE = 50;
     private static final int B_SKU_PRICE = 30;
     private static final int C_SKU_PRICE = 20;
     private static final int D_SKU_PRICE = 15;
     private static final int E_SKU_PRICE = 40;
+    private static final int F_SKU_PRICE = 10;
 
     @Test
     public void should_return_price_of_each_sku_when_only_one_item_is_in_basket() {
@@ -25,6 +27,7 @@ public class CheckoutTest {
         assertThat(Checkout.checkout(C_SKU), is(C_SKU_PRICE));
         assertThat(Checkout.checkout(D_SKU), is(D_SKU_PRICE));
         assertThat(Checkout.checkout(E_SKU), is(E_SKU_PRICE));
+        assertThat(Checkout.checkout(F_SKU), is(F_SKU_PRICE));
     }
 
     @Test
