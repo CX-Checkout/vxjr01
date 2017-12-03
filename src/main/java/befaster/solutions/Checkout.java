076e05b7d6a16275817c14a9a06b7f80e44b3d93
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 13:17:50 +0000

    Sun Dec 03 13:17:50 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -3,18 +3,25 @@ package befaster.solutions;
 import static java.util.Arrays.asList;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class Checkout {
+    private static final String A_SKU = "A";
+    private static final String B_SKU = "B";
+    private static final String C_SKU = "C";
+    private static final String D_SKU = "D";
     private static final HashMap<String, Integer> priceMap = new HashMap<String, Integer>() {{
-        put("A",50);
-        put("B",30);
-        put("C",20);
-        put("D",15);
+        put(A_SKU,50);
+        put(B_SKU,30);
+        put(C_SKU,20);
+        put(D_SKU,15);
     }};
 
     public static Integer checkout(String skus) {
-        skus.
-        return asList(skus.split("")).stream()
+        List<String> listOfSkus = asList(skus.split(""));
+
+
+        return (listOfSkus).stream()
                 .mapToInt((String sku) -> priceMap.get(sku))
                 .sum();
     }
