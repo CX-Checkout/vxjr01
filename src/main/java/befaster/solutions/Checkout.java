913da5c449cda660c45b56ebdd0da1eb477e83b2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 12:38:49 +0000

    Sun Dec 03 12:38:49 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,9 +1,16 @@
 package befaster.solutions;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
 
 public class Checkout {
+    private static final HashMap<String, Integer> priceMap = new HashMap<String, Integer>() {{
+        put("A",50);
+        put("B",30);
+        put("C",20);
+        put("D",15);
+    }};
+
     public static Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        return priceMap.get(skus);
     }
 }
--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -1,5 +1,18 @@
 package befaster.solutions;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+
 public class CheckoutTest {
+
+    @Test
+    public void should_return_price_of_each_sku_when_only_one_item_is_given() {
+        assertThat(Checkout.checkout("A"), is(50));
+        assertThat(Checkout.checkout("B"), is(30));
+        assertThat(Checkout.checkout("C"), is(20));
+        assertThat(Checkout.checkout("D"), is(15));
+    }
 
 }
