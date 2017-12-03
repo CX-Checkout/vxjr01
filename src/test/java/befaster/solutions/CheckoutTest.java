d961fd3148508647bf5903316304cfee9edac264
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 13:41:49 +0000

    Sun Dec 03 13:41:49 GMT 2017

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -39,13 +39,14 @@ public class CheckoutTest {
     public void should_return_sum_of_prices_for_given_skus_including_discounts_for_special_offers() {
         String skusWithDiscountForDoubleB = "BB";
         String skusWithDiscountForDoubleBMultipleTimes = "BBBBBBB";
-        String skusWithDiscountForDoubleBMultipleTimesWithOtherSkus = "BBBBBBB";
+        String skusWithDiscountForDoubleBWithOtherSkus = "DBDBDBDBDB";
         int discountForDoubleBSkuInBasket = 15;
         int totalCheckoutValueForBasket = 2 * B_SKU_PRICE - discountForDoubleBSkuInBasket;
         int totalCheckoutValueForDiscountedMultipleTimesBasket = 7 * B_SKU_PRICE - 3 * discountForDoubleBSkuInBasket;
-        int totalCheckoutValueForDiscountedMultipleTimesBasketWithOtherSkus = 7 * B_SKU_PRICE - 3 * discountForDoubleBSkuInBasket;
+        int totalCheckoutValueForDiscountedMultipleTimesBasketWithOtherSkus = 5 * D_SKU_PRICE +  5 * B_SKU_PRICE - 2 * discountForDoubleBSkuInBasket;
 
         assertThat(Checkout.checkout(skusWithDiscountForDoubleB), is(totalCheckoutValueForBasket));
         assertThat(Checkout.checkout(skusWithDiscountForDoubleBMultipleTimes), is(totalCheckoutValueForDiscountedMultipleTimesBasket));
+        assertThat(Checkout.checkout(skusWithDiscountForDoubleBWithOtherSkus), is(totalCheckoutValueForDiscountedMultipleTimesBasketWithOtherSkus));
     }
 }
