42e571228d3d316106313253f041ae9428ebb715
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 12:21:16 +0000

    Sun Dec 03 12:21:16 GMT 2017

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -2,6 +2,10 @@ package befaster.solutions;
 
 public class Hello {
     public static String hello(String friendName) {
-        return "Hello " + (friendName != null && !friendName.isEmpty() ? friendName : "World")+ "!";
+        return "Hello " + (friendNameIsGiven(friendName) ? friendName : "World")+ "!";
+    }
+
+    private static boolean friendNameIsGiven(String friendName) {
+        return friendName != null && !friendName.isEmpty();
     }
 }
