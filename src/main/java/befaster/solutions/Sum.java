18fce917be2b6fc796feeb2257a6a057151b6134
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 12:13:56 +0000

    Sun Dec 03 12:13:56 GMT 2017

--- src/main/java/befaster/solutions/Sum.java
+++ src/main/java/befaster/solutions/Sum.java
@@ -1,9 +1,11 @@
 package befaster.solutions;
 
+import befaster.runner.SolutionNotImplementedException;
+
 public class Sum {
 
     public static int sum(int x, int y) {
-        return x + y;
+        throw new SolutionNotImplementedException();
     }
 
 }
