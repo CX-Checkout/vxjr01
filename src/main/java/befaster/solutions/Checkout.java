c861a44e6d952093989e946fd00ce1b01f8559fb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 21:20:47 +0000

    Mon Dec 04 21:20:47 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -13,6 +13,26 @@ public class Checkout {
     private static final String D_SKU = "D";
     private static final String E_SKU = "E";
     private static final String F_SKU = "F";
+    private static final String G_SKU = "G";
+    private static final String H_SKU = "H";
+    private static final String I_SKU = "I";
+    private static final String J_SKU = "J";
+    private static final String K_SKU = "K";
+    private static final String L_SKU = "L";
+    private static final String M_SKU = "M";
+    private static final String N_SKU = "N";
+    private static final String O_SKU = "O";
+    private static final String P_SKU = "P";
+    private static final String Q_SKU = "Q";
+    private static final String R_SKU = "R";
+    private static final String S_SKU = "S";
+    private static final String T_SKU = "T";
+    private static final String U_SKU = "U";
+    private static final String V_SKU = "V";
+    private static final String W_SKU = "W";
+    private static final String X_SKU = "X";
+    private static final String Y_SKU = "Y";
+    private static final String Z_SKU = "Z";
 
     private static final HashMap<String, Integer> priceMap = new HashMap<String, Integer>() {{
         put(A_SKU, 50);
@@ -21,6 +41,26 @@ public class Checkout {
         put(D_SKU, 15);
         put(E_SKU, 40);
         put(F_SKU, 10);
+        put(G_SKU, 20);
+        put(H_SKU, 10);
+        put(I_SKU, 35);
+        put(J_SKU, 60);
+        put(K_SKU, 80);
+        put(L_SKU, 90);
+        put(M_SKU, 15);
+        put(N_SKU, 40);
+        put(O_SKU, 10);
+        put(P_SKU, 50);
+        put(Q_SKU, 30);
+        put(R_SKU, 50);
+        put(S_SKU, 30);
+        put(T_SKU, 20);
+        put(U_SKU, 40);
+        put(V_SKU, 50);
+        put(W_SKU, 20);
+        put(X_SKU, 90);
+        put(Y_SKU, 10);
+        put(Z_SKU, 50);
     }};
 
     private static final Integer discountForDoubleB = 15;
@@ -59,7 +99,7 @@ public class Checkout {
     }
 
     private static boolean containsValidSkus(String skus) {
-        return skus.matches("^[ABCDEF]*$");
+        return skus.matches("^[ABCDEFGHIJKLMNOUPQRSTUWXYZ]*$");
     }
 
 }
